83d76a7bd002068f864682f296614cb0608bc895
mejrakucevic <mejrakucevic420@gmail.com>
2024-01-25 14:01:56 +0100

    .

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,38 @@
 
 
 int main() {
+    // Ovo je komentar
+    std::cout << "Ja volim kafu!\n";
+    std::cout << "Volis li ti?" << std::endl;
+
+    int x = 19;
+    int y = 2;
+    int suma = x + y;
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+   
+
+    int godina = 2024;
+    double prosek = 9.1;
+    std::cout << prosek;
+
+    char ocena = 'A';
+    char inicijal = 'F' ;
+
+    std::cout << inicijal << '\n';
+
+    bool student = true;
+    bool forSale = false;
+
+    std::string ime = "Mejra";
+    std::cout << ime << '\n';
+
+    std::string dan = "Petak";
+    std::cout << dan << '\n';
+
+    std::cout << "Hello " << ime << ", You are " << x << " years old!";
      
+    const int WW1 = 1914;
 
      return 0;
 }
