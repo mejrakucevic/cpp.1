6e9acc2b4dca842532f3adf9e173f6ec6b8d0116
mejrakucevic <mejrakucevic420@gmail.com>
2024-02-18 15:33:01 +0100

    .

---
+++ zadaci.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// 7. make a program that asks for the user's full name, and prints the name on the screen
+// immediatly after 
+
+
+
+// int namePrint(string name) {
+//     cout << "Please enter your full name: ";
+//     getline(cin, name);
+//     cout << "Your name is: " << name;
+// }
+
+// int main() {
+//     string name;
+//     namePrint(name);
+//    return 0;
+// }
+
+// 8. Make a program that reads the names of three people in sequence, followed by their respective ages, and prints each name with their respective ages on the screen
+
+// int main() {
+//       string name1, name2, name3;
+//       int age1, age2, age3;
+//       cout << "Please enter first name: ";
+//       getline(cin, name1);
+
+//       cout << "Please enter first age: ";
+//       cin >> age1;
+
+//       cin.ignore();
+
+//       cout << "Please enter second name: ";
+//       getline(cin, name2);
+
+//       cout << "Please enter second age: ";
+//       cin >> age2;
+
+//       cin.ignore();
+
+//       cout << "Please enter third name: ";
+//       getline(cin, name3);
+
+//       cout << "Please enter third age: ";
+//       cin >> age3;
+
+//       cout << "People: \n";
+//       cout << name1 << ", age: " << age1 << '\n';
+//       cout << name2 << ", age: " << age2 <<'\n';
+//       cout << name3 << ", age: " << age3 <<'\n';
+
+
+//      return 0;
+
+// }
+
+// 9. Make a program that reads the names of three people in sequence, followed by their respective ages and heights, and prints the data in table format on the screen
+
+// int main() {
+//     string name1, name2, name3;
+//     int age1, age2, age3;
+//     float height1, height2, height3;
+
+//     cout << "Enter the name of the 1st person: ";
+//     getline(cin, name1);
+
+//     cout << "Enter the age of the 1st person: ";
+//     cin >> age1;
+
+//     cout << "Enter the height of the 1st person: ";
+//     cin >> height1;
+
+//     cin.ignore();
+
+//     cout << "\nEnter the name of the 2st person: ";
+//     getline(cin, name2);
+
+//     cout << "Enter the age of the 2st person: ";
+//     cin >> age2;
+
+//     cout << "Enter the height of the 2st person: ";
+//     cin >> height2;
+
+//     cin.ignore();
+
+//     cout << "\nEnter the name of the 3st person: ";
+//     getline(cin, name3);
+
+//     cout << "Enter the age of the 3st person: ";
+//     cin >> age3;
+
+//     cout << "Enter the height of the 3st person: ";
+//     cin >> height3;
+
+//     cout << "Name: " << name1 << " " << name2 << " " << name3 << '\n';
+//     cout << "Age: " << age1 << " " <<  age2 << " " <<  age3 << '\n';
+//     cout << "Height: " << height1 << " " <<  height2 << " " <<  height3;
+
+//     return 0;
+// }
+
+// 11. Write a program that prompts the user for two numbers and displays the addition, subtraction, multiplication, and division between them
+
+// int main() {
+//     float num1, num2;
+    
+//     cout << "Enter 1st number: ";
+//     cin >> num1;
+
+//     cout << "Enter 2nd number: ";
+//     cin >> num2;
+
+//     cout << "Addition: "<< num1 << " + " << num2 << " = " << num1 + num2 << '\n';
+//     cout << "Subtraction: "<< num1 << " - " << num2 << " = " << num1 - num2 << "\n";
+//     cout << "Multiplication: "<< num1 << " * " << num2 << " = " << num1 * num2 << "\n";
+//     cout << "Division: "<< num1 << " / " << num2 << " = " << num1 / num2 << "\n";
+
+// // 12. Arithmetic mean
+//     float arimeticka = (num1 + num2) / 2;
+//     cout << "Aritmethic mean: " << arimeticka;
+
+//     return 0;
+
+
+// }
+
+// 13.
