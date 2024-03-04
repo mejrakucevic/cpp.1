71df97d891502b50f30a4ba380d3163200764c60
mejrakucevic <mejrakucevic420@gmail.com>
2024-03-04 17:26:46 +0100

    .

--- DSAVezbe.cpp
+++ DSAVezbe.cpp
@@ -108,6 +108,35 @@ using namespace std;
 
 // RESENJE
 
-
+int main() {
+     int x = 121;
+    
+      while (x>0) {
+        int digit = x%10;
+        cout << digit;
+        x = x/10;
+      }
+    return 0;
+}
 
 // ----------------------------------------------------------------------------------------------------------------------------------
+
+// Merge Two Sorted Lists (21. Leetcode)
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// Example 1:
+//  Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
